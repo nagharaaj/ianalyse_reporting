b9ec5bfe5c96071ef684784ad40f6ccc229adddd
nagharaaj <37173342+nagharaaj@users.noreply.github.com>
2018-07-25 19:45:01 +0530

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -119,7 +119,7 @@ class AdminController extends AppController {
 
                 // NBR pitch status array id => pitch status
                 $arrPitchStatus = array();
-                $pitchStatusUrl = $siteUrl . "_api/web/lists(guid'c47bb064-faa5-4ab7-812c-3b005843314d')/items";
+                $pitchStatusUrl = $siteUrl . "_api/web/lists(guid'41a15a89-ddaa-476a-8277-dc391b69cfaf')/items";
                 curl_setopt( $ch, CURLOPT_URL, $pitchStatusUrl );
                 $pitchStatusContent = json_decode(curl_exec( $ch ));
                 $pitchStatusResult = $pitchStatusContent->d->results;
@@ -128,7 +128,7 @@ class AdminController extends AppController {
                 }
                 // NBR pitch stage array id => pitch stage
                 $arrPitchStage = array();
-                $pitchStageUrl = $siteUrl . "_api/web/lists(guid'eb47971c-2bf9-4ace-90f9-67d5117d9e31')/items";
+                $pitchStageUrl = $siteUrl . "_api/web/lists(guid'692736e7-8c0a-415e-9a73-2f33717b1a15')/items";
                 curl_setopt( $ch, CURLOPT_URL, $pitchStageUrl );
                 $pitchStageContent = json_decode(curl_exec( $ch ));
                 $pitchStageResult = $pitchStageContent->d->results;
