8a3965cadd729a48945ee7637b8893cc085abf76
nagharaaj <37173342+nagharaaj@users.noreply.github.com>
2018-10-25 15:18:37 +0530

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -56,18 +56,7 @@ class AdminController extends AppController {
         public function ad_hoc_reconciliation () {
 		
 		$HttpSocket = new HttpSocket();
-$response = $HttpSocket->post('https://accounts.accesscontrol.windows.net/6e8992ec-76d5-4ea5-8eae-b0c5e558749a/tokens/OAuth/2', [
-  'client_id' => '96d6293f-922a-4cb0-bbb1-38e58eb16008@6e8992ec-76d5-4ea5-8eae-b0c5e558749a',
-  'client_secret' => 'FXXI8/bRHbpNKjGSwFMb4kM5sRAJbNKUQ1b90b4nD44=',
-  "redirect_uri" => 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"],
-  'grant_type'=>'client_credentials',
-  'resource'=>'00000003-0000-0ff1-ce00-000000000000/globalappsportal.sharepoint.com@6e8992ec-76d5-4ea5-8eae-b0c5e558749a'
-  
-]);
-		$json = json_decode($response, true);
-$access_token= $json['access_token'];
-		
-		print_r($access_token);
+
 		
                 $currDt = date('Y-m-d h:i:s');
                 $lastDayDt = date('Y') . '-01-01';
