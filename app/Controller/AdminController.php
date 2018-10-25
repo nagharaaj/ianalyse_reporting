0c9503a3a16cd29d4f0822f47cd42cde92342cc5
nagharaaj <37173342+nagharaaj@users.noreply.github.com>
2018-10-25 12:44:50 +0530

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('CakeEmail', 'Network/Email');
+App::uses('HttpSocket', 'Network/Http');
 class AdminController extends AppController {
 	public $helpers = array('Html', 'Form');
         public $components = array('RequestHandler');
@@ -53,6 +54,38 @@ class AdminController extends AppController {
                 }
         }
         public function ad_hoc_reconciliation () {
+		
+		
+		$HttpSocket = new HttpSocket();
+$response = $HttpSocket->post('https://accounts.accesscontrol.windows.net/6e8992ec-76d5-4ea5-8eae-b0c5e558749a/tokens/OAuth/2', [
+  'client_id' => '96d6293f-922a-4cb0-bbb1-38e58eb16008@6e8992ec-76d5-4ea5-8eae-b0c5e558749a',
+  'client_secret' => 'FXXI8/bRHbpNKjGSwFMb4kM5sRAJbNKUQ1b90b4nD44=',
+  "redirect_uri" => 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"],
+        "grant_type" => 'client_credentials',
+  'grant_type'=>'client_credentials',
+  'resource'=>'00000003-0000-0ff1-ce00-000000000000/globalappsportal.sharepoint.com@6e8992ec-76d5-4ea5-8eae-b0c5e558749a',
+  "redirect_uri" => 'https://globalappsportal.sharepoint.com/_layouts/15/sharepoint.aspx?wa=wsignin1.0'
+
+]);
+		print_r($response);
+		$json = json_decode($response, true);
+$access_token= $json['access_token'];
+		
+		
+		$url="https://globalappsportal.sharepoint.com/sites/NBR/";
+
+
+   $res = $HttpSocket->GET($url."_API/Web/Lists('7797A8C0-8265-4B29-B2B9-DFFA9C304461')/Items",[
+
+'authorization'=> 'Bearer'. $access_token,
+'client_id' => '96d6293f-922a-4cb0-bbb1-38e58eb16008@6e8992ec-76d5-4ea5-8eae-b0c5e558749a',
+ 'resource'=>'00000003-0000-0ff1-ce00-000000000000/globalappsportal.sharepoint.com@6e8992ec-76d5-4ea5-8eae-b0c5e558749a',
+
+   ]);
+
+
+print_r($res);die;
+		
                 $currDt = date('Y-m-d h:i:s');
                 $lastDayDt = date('Y') . '-01-01';
                 $currTime = date('m/d/Y h:i:s');
