eaabcda47a8cfd65a724143869523df092759f27
nagharaaj <37173342+nagharaaj@users.noreply.github.com>
2018-10-25 16:24:38 +0530

    Update AdminController.php

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -56,11 +56,17 @@ class AdminController extends AppController {
         public function ad_hoc_reconciliation () {
 		
 		$HttpSocket = new HttpSocket();
-           $response = $HttpSocket->post('https://accounts.accesscontrol.windows.net/6e8992ec-76d5-4ea5-8eae-b0c5e558749a/tokens/OAuth/2',
-	'$client_id'='96d6293f-922a-4cb0-bbb1-38e58eb16008@6e8992ec-76d5-4ea5-8eae-b0c5e558749a'				 
-					);
+           $response = $HttpSocket->post('https://accounts.accesscontrol.windows.net/6e8992ec-76d5-4ea5-8eae-b0c5e558749a/tokens/OAuth/2',[
+		$params = array(
+        'response_type'=>'code',
+        "client_id" =>'96d6293f-922a-4cb0-bbb1-38e58eb16008@4345a7b9-9a63-4910-a426-35363201d503' ,
+        "client_secret" => 'FXXI8/bRHbpNKjGSwFMb4kM5sRAJbNKUQ1b90b4nD44=',
+        "grant_type" => 'client_credentials',
+        'resource'=>'00000003-0000-0ff1-ce00-000000000000/globalappsportal.sharepoint.com@6e8992ec-76d5-4ea5-8eae-b0c5e558749a'
+    )]);
 		
-		print_r($response);die;
+		
+		
                 $currDt = date('Y-m-d h:i:s');
                 $lastDayDt = date('Y') . '-01-01';
                 $currTime = date('m/d/Y h:i:s');
